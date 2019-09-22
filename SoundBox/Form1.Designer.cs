b3dc3da6d4eb9899afc3b28240f1b1b352a17dd8
BENDAHMANE Mounir <mounir.bendahmane@ecole-isitech.fr>
2019-09-22 16:06:14 +0200

    commit

---
+++ SoundBox/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace SoundBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(554, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 92);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Z";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(377, 207);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 92);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Q";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(735, 207);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(164, 92);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "D";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(735, 109);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(164, 92);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "E";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(554, 305);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(164, 92);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "X";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(735, 308);
+            this.button8.Name = "button8";
+            this.button8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button8.Size = new System.Drawing.Size(164, 92);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "C";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(377, 305);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(158, 92);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "W";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(735, 406);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(164, 32);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Play(mp3)";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(377, 435);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(158, 32);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Start record";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Button11_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(377, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(522, 91);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(554, 435);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(164, 32);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Stop record";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Button12_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(560, 207);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 92);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "S";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click_1);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(735, 482);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(164, 32);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "Pause";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(735, 520);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(164, 32);
+            this.button14.TabIndex = 15;
+            this.button14.Text = "Stop";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(735, 444);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(164, 32);
+            this.button15.TabIndex = 16;
+            this.button15.Text = "Resume";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(554, 473);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(158, 32);
+            this.button16.TabIndex = 17;
+            this.button16.Text = "Convert to mp3";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.Button16_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1322, 708);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "A";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+    }
+}
+
---
+++ SoundBox/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NAudio.Lame;
+using NAudio.Wave;
+
+namespace SoundBox
+{
+    public partial class Form1 : Form
+    {
+        private const string SoundLocation = @"C:\Users\Mounir\source\repos\SoundBox\SoundBox\Sounds\Aoe2\blacksmith.wav";
+        private const string BlacksmithFilename = "blacksmith.wav";
+        private WaveFileWriter RecordedAudioWriter = null;
+        private WasapiLoopbackCapture CaptureInstance = null;
+        private WaveOut waveOut = null;
+        string outputFilePath = @"C:\Users\";
+        string inputFileWav = "";
+        string outputFilePathMP3 = @"C:\Users\";
+        string lectureFilePath = @"";
+        private Mp3FileReader reader = null;
+        static System.Windows.Forms.Timer s_myTimer = new System.Windows.Forms.Timer();
+        // Resources
+        /*        Stream str = Properties.Resources.mySoundFile;
+                RecordPlayer rp = new RecordPlayer();
+                rp.Open(new WaveReader(str));
+          rp.Play();*/
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            button12.Enabled = false;
+            button13.Enabled = false;
+            button14.Enabled = false;
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.donkey);
+            player.Play();
+        }
+
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.wonder);
+            player.Play();
+        }
+
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.blacksmith);
+            player.Play();
+        }
+
+        // Stop and save recording
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            this.CaptureInstance.RecordingStopped += (s, a) =>
+            {
+                this.RecordedAudioWriter.Dispose();
+                this.RecordedAudioWriter = null;
+                CaptureInstance.Dispose();
+            };
+
+            this.CaptureInstance.StopRecording();
+            this.BackgroundImage = null;
+            button11.Enabled = true;
+            button12.Enabled = false;
+
+        }
+
+        // Start recordiing
+        private void Button11_Click(object sender, EventArgs e)
+        {
+           
+            SaveFileDialog savefiledialog = new SaveFileDialog();
+            savefiledialog.InitialDirectory = "C:\\";
+            savefiledialog.RestoreDirectory = true;
+            savefiledialog.Title = "Choisissez un titre";
+            savefiledialog.DefaultExt = ".wav";
+            savefiledialog.Filter = "Wav Files (*.wav)|*wav";
+            if (savefiledialog.ShowDialog() == DialogResult.OK)
+            {
+                this.outputFilePath = savefiledialog.FileName;
+            }
+            // savefiledialog.FileOk += new System.ComponentModel.CancelEventHandler(SaveFileDialog1_FileOk);
+            this.CaptureInstance = new WasapiLoopbackCapture();
+
+            // Redefine the audio writer instance with the given configuration
+            this.RecordedAudioWriter = new WaveFileWriter(outputFilePath, CaptureInstance.WaveFormat);
+
+            this.CaptureInstance.DataAvailable += (s, a) =>
+            {
+                this.RecordedAudioWriter.Write(a.Buffer, 0, a.BytesRecorded);
+            };
+
+            this.CaptureInstance.StartRecording();
+            button11.Enabled = false;
+            button12.Enabled = true;
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(SoundBox.Resources.blacksmith);
+                    player.Play();
+                    break;
+                case Keys.Z:
+                    player = new System.Media.SoundPlayer(SoundBox.Resources.donkey);
+                    player.Play();
+                    break;
+                case Keys.E:
+                    player = new System.Media.SoundPlayer(SoundBox.Resources.wonder);
+                    player.Play();
+                    break;
+                case Keys.Q:
+                    player = new System.Media.SoundPlayer(SoundBox.Resources.attackwarning);
+                    player.Play();
+                    break;
+                case Keys.S:
+                    player = new System.Media.SoundPlayer(SoundBox.Resources.victory);
+                    player.Play();
+                    break;
+                case Keys.D:
+                    player = new System.Media.SoundPlayer(SoundBox.Resources.monastery);
+                    player.Play();
+                    break;
+                case Keys.W:
+                    player = new System.Media.SoundPlayer(SoundBox.Resources.barracks);
+                    player.Play();
+                    break;
+                case Keys.X:
+                    player = new System.Media.SoundPlayer(SoundBox.Resources.camelselect1);
+                    player.Play();
+                    break;
+                case Keys.C:
+                    player = new System.Media.SoundPlayer(SoundBox.Resources.castle);
+                    player.Play();
+                    break;
+
+            }
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.attackwarning);
+            player.Play();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.monastery);
+            player.Play();
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.barracks);
+            player.Play();
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.camelselect1);
+            player.Play();
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.castle);
+            player.Play();
+        }
+
+        private void Button4_Click_1(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.attackwarning);
+            player.Play();
+        }
+
+        private void Button3_Click_1(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resources.victory);
+            player.Play();
+        }
+
+
+        // Play a wav file
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.InitialDirectory = "C:\\";
+            openfiledialog.RestoreDirectory = true;
+            openfiledialog.Title = "Choisissez un fichier";
+            openfiledialog.DefaultExt = ".mp3";
+            openfiledialog.Filter = "Mp3 Files (*.mp3)|*mp3";
+            if (openfiledialog.ShowDialog() == DialogResult.OK)
+            {
+                lectureFilePath = openfiledialog.FileName;
+                waveOut = new WaveOut();
+                var reader = new Mp3FileReader(lectureFilePath);
+            }
+            reader = new Mp3FileReader(lectureFilePath);
+            button13.Enabled = true;
+            button14.Enabled = true;
+        }
+
+
+        // PAUSE 
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            waveOut.Pause();
+        }
+
+
+        // STOP 
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            button13.Enabled = false;
+            button14.Enabled = false;
+        }
+
+        // RE
+        private void Button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // COnvert to mp3
+        private void Button16_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefiledialog1 = new SaveFileDialog();
+            savefiledialog1.InitialDirectory = "C:\\";
+            savefiledialog1.RestoreDirectory = true;
+            savefiledialog1.Title = "Choisissez un titre";
+            savefiledialog1.DefaultExt = ".wav";
+            savefiledialog1.Filter = "Wav Files (*.wav)|*wav";
+
+            if (savefiledialog1.ShowDialog() == DialogResult.OK)
+            {
+                inputFileWav = savefiledialog1.FileName;
+            }
+            ConvertWavToMp3(inputFileWav, outputFilePath);
+        }
+
+        public void ConvertWavToMp3(string WavFile, string outPutFile)
+        {
+
+            WaveFileReader rdr = new WaveFileReader(WavFile);
+            using (var wtr = new NAudio.Lame.LameMP3FileWriter(outPutFile, rdr.WaveFormat, 128))
+            {
+                rdr.CopyTo(wtr);
+                rdr.Dispose();
+                wtr.Dispose();
+                return;
+            }
+        }
+    }
+}
